0f57eb2e78563922e23b55e241af7e756bd9d70c
alexander ames <alexanderthomasames@gmail.com>
2017-02-24 15:21:59 -0700

    database dump

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+
+
 require_relative 'boot'
 
 require "rails"
@@ -21,5 +23,6 @@ module Shine
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  	config.active_record.schema_format = :sql
   end
 end
