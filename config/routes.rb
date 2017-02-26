02db1306fa9ea926ab4f7821627f1090e12efccc
alexander ames <alexanderthomasames@gmail.com>
2017-02-25 20:04:35 -0700

    create search UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	resources :customers, only: [ :index ]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
