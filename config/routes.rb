7ccaf68be732e130c896901e43f4e13fca3d8e6e
alexander ames <alexanderthomasames@gmail.com>
2017-02-28 12:07:29 -0700

    configure rails and angular

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 	resources :customers, only: [ :index ]
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "angular_test", to: "angular_test#index"
 
 	root 'dashboard#index'
 
