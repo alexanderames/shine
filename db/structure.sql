d1f6b0382d6f30b3f3bfce10f0ceae80cbc3ef8b
alexander ames <alexanderthomasames@gmail.com>
2017-02-27 19:38:34 -0700

    Index derived and partial values.

--- db/structure.sql
+++ db/structure.sql
@@ -177,6 +177,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -213,6 +234,7 @@ SET search_path TO "$user", public;
 INSERT INTO schema_migrations (version) VALUES
 ('20170224181046'),
 ('20170224204410'),
-('20170224225842');
+('20170224225842'),
+('20170228004750');
 
 
