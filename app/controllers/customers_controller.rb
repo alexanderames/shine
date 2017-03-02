6704dba9cd87c884e387726157c2a52af5466688
alexander ames <alexanderthomasames@gmail.com>
2017-03-01 18:00:05 -0700

    insert Angular directives

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,10 +2,9 @@ class CustomersController < ApplicationController
   PAGE_SIZE = 10
 
   def index
-    @page = (params[:page] || 0).to_i
 
     # ...
-    
+    @page = (params[:page] || 0).to_i
     if params[:keywords].present?
       @keywords = params[:keywords]
       customer_search_term = CustomerSearchTerm.new(@keywords)
@@ -16,6 +15,10 @@ class CustomersController < ApplicationController
         offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @customers = []
+    end
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @customers }
     end
   end
 end
